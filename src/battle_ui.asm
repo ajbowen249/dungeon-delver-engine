ec421eec865030490ed0ad1caeab2f7ca31ce260
Alex Bowen <ajbowen249@gmail.com>
2024-02-02 17:55:02 -0500

    feat: determine turn order from initiative

--- src/battle_ui.asm
+++ src/battle_ui.asm
@@ -5,6 +5,7 @@ blank_message_string: .asciz "                               "
 #define combatants_first_row 2
 
 initiative_header: .asciz "Initiative:"
+order_header: .asciz "Turn Order:"
 
 screen_data: .dw 0
 
@@ -37,6 +38,28 @@ enemy_combatants:
 
 general_counter: .db 0
 
+; associative array of initiative roll->player index
+; will be sorted by initiative once init_screen is complete
+.macro ALLOCATE_INITIATIVE
+.db 0
+.db 0
+.endm
+
+initiative_order:
+    ALLOCATE_INITIATIVE
+    ALLOCATE_INITIATIVE
+    ALLOCATE_INITIATIVE
+    ALLOCATE_INITIATIVE
+    ALLOCATE_INITIATIVE
+    ALLOCATE_INITIATIVE
+    ALLOCATE_INITIATIVE
+    ALLOCATE_INITIATIVE
+
+initiative_sort_space:
+    ALLOCATE_INITIATIVE
+
+total_number_of_combatants: .db 0
+
 ; Displays the combat screen until the encounter is resolved
 ; HL should contain a pointer to an array of player data for the controlled party, and A the size of the party
 ; BC should contain a pointer to an array of player data for the enemy party, and D the number of enemies
@@ -52,6 +75,12 @@ battle_ui::
 
     ld a, 0
     ld (should_exit), a
+
+    ld a, (party_size)
+    ld b, a
+    ld a, (enemy_party_size)
+    add a, b
+    ld (total_number_of_combatants), a
 
     call init_screen
 
@@ -110,6 +139,7 @@ read_loop_continue:
 init_screen:
     call initialize_combatants
     call display_initiative_results
+    call display_initiative_order
     call rom_clear_screen
     call draw_combatants
     ret
@@ -141,23 +171,46 @@ clear_message_area:
     PRINT_AT_LOCATION 8, message_column, blank_message_string
     ret
 
+initialize_index: .db 0
 initialize_combatants:
+    ld a, 0
+    ld (initialize_index), a
     ld hl, initialize_combatants_foreach_callback
     call for_each_combatant
+
+    ld d, 2
+    ld a, (total_number_of_combatants)
+    ld bc, initiative_sort_space
+    ld hl, initiative_order
+    call sort_associative_array
+
     ret
 
+initiative_result: .db 0
 initialize_combatants_foreach_callback:
     ; roll initiative
     ld hl, (foreach_player_address)
     call roll_dexterity_check
+    ld (initiative_result), a
 
     ld hl, (foreach_combat_address)
     ld b, 0
     ld c, cbt_offs_initiative
     add hl, bc
-    ld b, 0
-    ld c, a; from roll_dexterity_check
-    ld (hl), bc
+    ld a, (initiative_result)
+    ld (hl), a
+
+    ; save initiative for future sort
+    ld a, (initialize_index)
+    ld b, 2
+    ld hl, initiative_order
+    call get_array_item
+
+    ld a, (initiative_result)
+    ld (hl), a
+    inc hl
+    ld a, (initialize_index)
+    ld (hl), a
 
     ld hl, (foreach_combat_address)
     ld b, 0
@@ -176,8 +229,7 @@ initialize_combatants_foreach_callback:
     add hl, bc
     ld a, cbt_initial_party_flags
     ld (hl), a
-
-    ret
+    jp initialize_combatants_foreach_callback_done
 
 init_enemy_combatant:
     ld hl, (foreach_combat_address)
@@ -186,6 +238,11 @@ init_enemy_combatant:
     add hl, bc
     ld a, cbt_initial_enemy_flags
     ld (hl), a
+
+initialize_combatants_foreach_callback_done:
+    ld a, (initialize_index)
+    inc a
+    ld (initialize_index), a
     ret
 
 combat_row_buffer: .asciz "      "
@@ -346,6 +403,70 @@ for_each_&LABEL_combatant:
 
     ret
 
+initiative_counter: .db 0
+display_initiative_order:
+    call rom_clear_screen
+
+    PRINT_AT_LOCATION 1, 1, order_header
+
+    ld a, 0
+    ld (initiative_counter), a
+
+display_initiative_order_loop:
+    ld a, (initiative_counter)
+    inc a
+
+    ld h, 24
+    ld l, a
+    call rom_set_cursor
+
+    ld hl, initiative_order
+    ld a, (initiative_counter)
+    ld b, 2
+    call get_array_item
+    ld a, (hl)
+
+    ld d, 0
+    ld e, a
+    call de_to_decimal_string
+
+    ld hl, bc
+    call print_string
+
+    ld a, (initiative_counter)
+    inc a
+
+    ld h, 14
+    ld l, a
+    call rom_set_cursor
+
+    ld hl, initiative_order
+    ld a, (initiative_counter)
+    ld b, 2
+    call get_array_item
+    inc hl
+    ld a, (hl)
+
+    call get_character_at_index_a
+    ld b, 0
+    ld c, pl_offs_name
+    add hl, bc
+    call print_string
+
+    ld a, (initiative_counter)
+    inc a
+    ld (initiative_counter), a
+    ld b, a
+    ld a, (total_number_of_combatants)
+    cp a, b
+    jp nz, display_initiative_order_loop
+
+initiative_order_wait_loop:
+    call rom_kyread
+    jp z, initiative_order_wait_loop
+
+    ret
+
 initiative_display_counter: .db 0
 display_initiative_results:
     call rom_clear_screen
@@ -397,6 +518,30 @@ display_initiative_results_foreach_callback:
     inc a
     ld (initiative_display_counter), a
 
+    ret
+
+get_character_at_index_a:
+    ld b, a
+    ld a, (party_size)
+    dec a
+
+    cp a, b
+    jp m, get_character_from_enemy_list
+
+    ld a, pl_data_size
+    ld hl, (party_location)
+    call get_array_item
+    ret
+get_character_from_enemy_list:
+    ld a, (party_size)
+    ld c, a
+    ld a, b
+    sub a, c
+
+    ld b, a
+    ld a, pl_data_size
+    ld hl, (enemy_party_location)
+    call get_array_item
     ret
 
 call_hl:
