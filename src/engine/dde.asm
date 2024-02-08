8d89959704457bc7e77d1472d42112e09c56ccea
Alex Bowen <ajbowen249@gmail.com>
2024-02-07 19:10:17 -0500

    feat: add version header

--- src/engine/dde.asm
+++ src/engine/dde.asm
@@ -1,6 +1,11 @@
 ; Dungeon Delver Engine
 ; This is a CRPG engine that implements a subset of OGL SRD 5.1
 
+dde_version:
+dde_version_major: .db 0
+dde_version_minor: .db 0
+dde_version_build: .db 1
+
 #include "math.asm"
 #include "util.asm"
 #include "array_util.asm"
