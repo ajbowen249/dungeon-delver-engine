226d17c5d9c5ad9e1d665562b50ba15ed7e8b988
Alex Bowen <ajbowen249@gmail.com>
2024-02-20 12:41:34 -0500

    feat: flip move/attack options

--- src/engine/battle_ui/battle_menu_options.asm
+++ src/engine/battle_ui/battle_menu_options.asm
@@ -1,8 +1,8 @@
-#define bm_option_move_value $01
-opt_bm_option_move_label: .asciz "Move"
-
-#define bm_option_attack_value $02
+#define bm_option_attack_value $01
 opt_bm_option_attack_label: .asciz "Attack"
+
+#define bm_option_move_value $02
+opt_bm_option_move_label: .asciz "Move"
 
 #define bm_option_inspect_value $FD
 opt_bm_option_inspect_label: .asciz "Inspect"
@@ -11,13 +11,13 @@ opt_bm_option_inspect_label: .asciz "Inspect"
 opt_bm_option_end_turn_label: .asciz "End Turn"
 
 opt_bm_root:
-.db bm_option_move_value
-bm_root_move_flags: .db default_options_flags
-.dw opt_bm_option_move_label
-
 .db bm_option_attack_value
 bm_root_attack_flags: .db default_options_flags
 .dw opt_bm_option_attack_label
+
+.db bm_option_move_value
+bm_root_move_flags: .db default_options_flags
+.dw opt_bm_option_move_label
 
 .db bm_option_inspect_value
 .db default_options_flags
