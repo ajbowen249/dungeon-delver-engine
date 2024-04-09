a493ec0ac4a7435eba70a73708e845b111b2809a
Alex Bowen <ajbowen249@gmail.com>
2024-04-09 10:41:43 -0400

    refactor: update test app with new interactables pattern

--- src/apps/test_campaign/screens/screen_2.asm
+++ src/apps/test_campaign/screens/screen_2.asm
@@ -24,16 +24,16 @@ screen_2_title: .asciz "Test Room 2"
 screen_2_start_x: .db 2 ; 1-indexed since it's screen coordinates!
 screen_2_start_y: .db 2
 screen_2_interactables:
-    DEFINE_INTERACTABLE door_1, in_door, $01, 2, 1
-    DEFINE_INTERACTABLE blank_1, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_3, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_4, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_5, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_6, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_7, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_8, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_9, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_0, 0, 0, 0, 0
+    DEFINE_INTERACTABLE door_1, in_door, iflags_door, 2, 1
+    DEFINE_INTERACTABLE blank_1, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_3, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_4, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_5, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_6, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_7, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_8, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_9, 0, iflags_normal, 0, 0
+    DEFINE_INTERACTABLE blank_0, 0, iflags_normal, 0, 0
 screen_2_get_interaction_prompt: .dw get_interaction_prompt
 screen_2_interact_callback: .dw on_interact
 screen_2_menu_callback: .dw main_menu
