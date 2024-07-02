874f8ef7f82a51df6b556a7f923c52c86d4fb20e
Alex Bowen <ajbowen249@gmail.com>
2024-07-01 22:00:51 -0400

    feat: initial interactables panel

--- tools/editor/common.py
+++ tools/editor/common.py
@@ -2,6 +2,7 @@ import os
 from tkinter import PhotoImage
 
 ICON_PATH = os.path.join(os.path.dirname(__file__), 'icon.png')
+FONT = ('Arial', 15)
 
 def get_app_icon():
     return PhotoImage(file=ICON_PATH)
