b7bff6a8c1d61071dc20b60b257e7bf5aae7af7d
Alex Bowen <ajbowen249@gmail.com>
2024-07-02 18:12:26 -0400

    feat: add initial interactables panel

--- tools/editor/interactables_panel.py
+++ tools/editor/interactables_panel.py
@@ -1,13 +1,55 @@
 from tkinter import *
+from typing import Callable
 
 from tools.editor.common import FONT
+from tools.editor.text_field import TextField
 from tools.constants import MAX_INTERACTABLES
-from tools.dde_project import DDEScreen
+from tools.dde_project import DDEScreen, DDEInteractable
+
+class BaseInteractableProps(Frame):
+    def __init__(self, root: Misc, interactable: DDEInteractable, update_label: Callable[[str], None]):
+        super().__init__(root)
+        self.label_field = TextField(self, 'Label', interactable, 'label')
+        self.label_field.grid(row=0, column=0)
+        self.label_field.add_observer(lambda s: update_label(s))
+
+        self.type_field = TextField(self, 'Type', interactable, 'type')
+        self.type_field.grid(row=1, column=0)
+
+        self.flags_field = TextField(self, 'Flags', interactable, 'flags')
+        self.flags_field.grid(row=2, column=0)
+
+        self.prompt_field = TextField(self, 'Prompt Label', interactable, 'prompt_label')
+        self.prompt_field.grid(row=3, column=0)
+
+        location_frame = Frame(self)
+        location_frame.grid(row=4, column=0)
+        location = interactable.location if interactable is not None else None
+
+        self.x_field = TextField(location_frame, 'X', location, 'col', width=9)
+        self.x_field.pack(side='left')
+        self.y_field = TextField(location_frame, 'Y', location, 'row', width=9)
+        self.y_field.pack(side='left')
+
+    def set_interactable(self, interactable: DDEInteractable):
+        self.label_field.rebind(interactable)
+        self.type_field.rebind(interactable)
+        self.flags_field.rebind(interactable)
+        self.prompt_field.rebind(interactable)
+
+        location = interactable.location if interactable is not None else None
+        self.x_field.rebind(location)
+        self.y_field.rebind(location)
+
+    def focus_label(self):
+        self.label_field.focus_entry()
 
 class InteractablesPanel(Frame):
     def __init__(self, root: Misc, screen: DDEScreen):
         super().__init__(root, padx=10)
         self.screen = screen
+
+        self.selected_interactable: DDEInteractable | None = None
 
         self.add_button = Button(self, text='+')
         self.add_button.grid(row=0, column=0)
@@ -21,10 +63,22 @@ class InteractablesPanel(Frame):
         self.listbox.grid(row=1, column=2)
         self.listbox.bind('<<ListboxSelect>>', lambda e: self.on_selected_interactable_changed())
 
+        self.edit_panel = Frame(self)
+        self.edit_panel.grid(row = 2, column=0, columnspan=3)
+
+        self.base_props_panel = BaseInteractableProps(self.edit_panel, None, lambda v: self.update_selected_label())
+        self.base_props_panel.pack()
+
     def on_selected_interactable_changed(self):
         selection = self.listbox.curselection()
         self.remove_button.configure(state='disabled' if selection == () else 'normal')
         self.add_button.configure(state='normal' if len(self.screen.interactables) < MAX_INTERACTABLES else 'disabled')
+
+        if selection == ():
+            return
+
+        self.selected_interactable = self.screen.interactables[selection[0]]
+        self.base_props_panel.set_interactable(self.selected_interactable)
 
     def delete_selected_interactable(self):
         selection = self.listbox.curselection()
@@ -44,3 +98,12 @@ class InteractablesPanel(Frame):
             self.listbox.insert(i, interactable.label)
 
         self.on_selected_interactable_changed()
+
+    def update_selected_label(self):
+        selection = self.listbox.curselection()
+        if selection == ():
+            return
+
+        self.build_list()
+        self.listbox.selection_set(selection[0])
+        self.base_props_panel.focus_label()
--- tools/editor/text_field.py
+++ tools/editor/text_field.py
@@ -12,17 +12,53 @@ class TextField(Frame):
             self,
             root: Misc,
             label: str,
-            source_object: object,
+            source_object: object | None,
             source_attr: str,
-            converter: Callable[[str], any] = lambda v: v
+            converter: Callable[[str], any] = lambda v: v,
+            **kwargs
         ):
         super().__init__(root)
-        var = StringVar(self, str(getattr(source_object, source_attr)))
+        self.source_object = source_object
+        self.source_attr = source_attr
+        self.converter = converter
 
-        def set_prop():
-            setattr(source_object, source_attr, converter(var.get()))
+        self.var = StringVar(self, self.get_source_str())
+        self.observers: list[Callable[[str], None]] = []
 
-        var.trace_add('write', lambda v, i, m: set_prop())
+        self.var.trace_add('write', lambda v, i, m: self.set_prop())
 
         Label(self, text=label).grid(row=0, column=0, sticky=W)
-        Entry(self, textvariable=var).grid(row=1, column=0)
+        self.entry = Entry(self, textvariable=self.var, width=kwargs.get('width', None))
+        self.entry.grid(row=1, column=0)
+        self.update_enablement()
+
+    def get_source_str(self):
+        # None source_object is "unbound" state
+        if self.source_object is None:
+            return ''
+
+        return str(getattr(self.source_object, self.source_attr))
+
+    def set_prop(self):
+        # We also disengage from the source object so we can update the visual binding with our new value
+        if self.source_object is None:
+            return
+
+        setattr(self.source_object, self.source_attr, self.converter(self.var.get()))
+        for observer in self.observers:
+            observer(self.var.get())
+
+    def add_observer(self, observer: Callable[[str], None]):
+        self.observers.append(observer)
+
+    def focus_entry(self):
+        self.entry.focus_set()
+
+    def rebind(self, new_source: object):
+        self.source_object = None
+        self.var.set(str(getattr(new_source, self.source_attr)))
+        self.source_object = new_source
+        self.update_enablement()
+
+    def update_enablement(self):
+        self.entry.configure(state='normal' if self.source_object is not None else 'disabled')
